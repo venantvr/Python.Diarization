bcbb1c42c888f319455941236c6e27f6feea665c
venantvr <r.venant.valery@gmail.com>
2022-09-14 22:01:28 +0200

    First commit...

--- diarizers/custom_annotation.py
+++ diarizers/custom_annotation.py
@@ -24,14 +24,15 @@ class CustomAnnotation(Annotation):
                 end = tuple_track.end * 1000
                 end = min(floor(audio.duration_seconds * 1000), floor(end + 1000))
                 audio_chunk = audio[start:end]
-                chunk = "{0}{1}{2}".format(results, os.sep, Path(audio_path).stem)
+                stem = Path(audio_path).stem
+                chunk = "{0}{1}{2}".format(results, os.sep, stem)
                 name = "{0}{1}{2}".format(chunk, os.sep, who)
                 try:
                     os.makedirs(name)
                 except OSError:
-                    print("Creation of the directory %s failed" % name)
+                    pass  # print("Creation of the directory %s failed" % name)
                 else:
-                    print("Successfully created the directory %s" % name)
-                out_f = "{0}{1}{2}-{3}.wav" \
-                    .format(name, os.sep, str(floor(start)).zfill(7), str(floor(end)).zfill(7))
+                    pass  # print("Successfully created the directory %s" % name)
+                out_f = "{0}{1}{2}-{3}-{4}.wav" \
+                    .format(name, os.sep, stem, str(floor(start)).zfill(7), str(floor(end)).zfill(7))
                 audio_chunk.export(out_f, format="wav")
